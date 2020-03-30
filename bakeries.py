0f2afeff782bb5912cdb5f8829b4bf6eb5e296f5
Sana Wiqas <56367400+sanaw877@users.noreply.github.com>
2020-03-30 19:49:12 -0400

    Update bakeries.py

--- bakeries.py
+++ bakeries.py
@@ -7,6 +7,8 @@ from selenium.webdriver.common.keys import Keys
 import os
 import pandas as pd
 import re
+from bs4 import BeautifulSoup
+import requests
 
 os.chdir(r'/Users/sundaswiqas/Downloads')
 # PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
@@ -14,29 +16,50 @@ os.chdir(r'/Users/sundaswiqas/Downloads')
 
 bakeries = pd.read_csv("Bakeries_in_NYC.csv")
 bakeries['DBA'] = bakeries['DBA'].str.lower()
-drop_kws = 'cookie|cake|cupcake|cafe|patisserie|confection|pastry|sweet|ritz carlton'
+drop_kws = 'cookie|cake|cupcake|cafe|patisserie|confection|pastry|sweet|ritz carlton|cinnabon'
 bakeries = bakeries[~bakeries['DBA'].str.contains(drop_kws,regex=True)]
+# bakeries.to_excel('bakeries_list.xlsx')
 
+bakeries = bakeries['DBA'].to_list()
 
 browser = webdriver.Chrome('/Users/sundaswiqas/Desktop/chromedriver')
-browser.get('http://www.google.com')
-search_box = browser.find_element_by_name('q')
-search_box.send_keys('maison kayser')
-search_box.submit()
 
 
-# for bakery in bakeries['DBA']:
-# 	search_box.send_keys(bakery)
-# 	search_box.submit()
+for bakery in bakeries[3:5]:
+	browser.get('http://www.google.com')
+	search_box = browser.find_element_by_name('q')
+	search_box.send_keys(bakery)
+	search_box.submit()
 
-results = browser.find_elements_by_xpath('//div[@class="r"]/a/h3')
-results[0].click()
-for a in browser.find_elements_by_tag_name('a'):
-	print(a.text)
+	try:
+	# results = browser.find_elements_by_xpath('//div[@class="r"]/a/h3')
+		links = browser.find_elements_by_css_selector('div.r > a')
+		first_result = links[0].get_attribute("href")
 
+		time.sleep(5)
+
+		headers = {'User Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
+		session = requests.Session()
+		page = session.get(first_result, headers=headers)
+	except:
+		pass
+	
+	if page.status_code == 200:
+
+		soup = BeautifulSoup(page.content, 'html.parser')
+		# print(soup.prettify())
+
+		hrefs = []
+		for link in soup.find_all('a'):
+				href = link.get('href')
+				if href:
+					hrefs.append(href)
+		
+		if hrefs:		
+			for href in hrefs:
+				if 'menu' in href:
+					href = soup.find_element_by_link_text()
+
+	time.sleep(10)
+	
 browser.quit()
-# txt = browser.find_elements_by_tag_name('p')
-
-# for elem in txt:
-# 	print(elem.text)
-# browser.quit()
